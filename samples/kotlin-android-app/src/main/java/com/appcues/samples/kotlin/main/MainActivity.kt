b1ae6fce16a0fff14b4f1a0ffac4ac0cce6d562f
James Ellis <james@appcues.com>
2022-01-21 09:58:40 -0500

    👌 Updates from code review

--- samples/kotlin-android-app/src/main/java/com/appcues/samples/kotlin/main/MainActivity.kt
+++ samples/kotlin-android-app/src/main/java/com/appcues/samples/kotlin/main/MainActivity.kt
@@ -12,12 +12,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
