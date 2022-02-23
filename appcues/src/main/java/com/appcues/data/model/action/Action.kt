31ddbf7e76ebb69e5e489c2cfaab87a9eb3a2338
James Ellis <james@appcues.com>
2022-02-23 10:15:13 -0500

    ♻️ Remove Parcelable code on Experience models

--- appcues/src/main/java/com/appcues/data/model/action/Action.kt
+++ appcues/src/main/java/com/appcues/data/model/action/Action.kt
@@ -1,10 +1,6 @@
 package com.appcues.data.model.action
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 internal data class Action(
     val on: OnAction,
     val type: String,
-) : Parcelable
+)
--- appcues/src/main/java/com/appcues/data/model/styling/ComponentShadow.kt
+++ appcues/src/main/java/com/appcues/data/model/styling/ComponentShadow.kt
@@ -1,12 +1,8 @@
 package com.appcues.data.model.styling
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 internal data class ComponentShadow(
     val color: ComponentColor,
     val radius: Int = 0,
     val x: Int = 0,
     val y: Int = 0,
-) : Parcelable
+)
