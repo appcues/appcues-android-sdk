b81ecd988aeeeaf0adbcbdfd00df987ad7177db3
Andre Tortolano <andret@appcues.com>
2022-03-18 10:48:25 -0300

    🐛 Fixed Skippable trait icon for different backgrounds

--- appcues/src/main/java/com/appcues/trait/appcues/SkippableTrait.kt
+++ appcues/src/main/java/com/appcues/trait/appcues/SkippableTrait.kt
@@ -1,6 +1,6 @@
 package com.appcues.trait.appcues
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Spacer
@@ -8,13 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -39,16 +37,14 @@ internal class SkippableTrait(
                     .align(Alignment.TopEnd)
                     .padding(8.dp)
                     .clip(CircleShape)
-                    .size(30.dp, 30.dp)
-                    .background(Color(color = 0x80000000)),
+                    .size(30.dp, 30.dp),
                 onClick = {
                     stateMachine.handleAction(EndExperience())
                 }
             ) {
-                Icon(
-                    painter = painterResource(id = android.R.drawable.ic_menu_close_clear_cancel),
+                Image(
+                    painter = painterResource(id = R.drawable.ic_dismiss),
                     contentDescription = stringResource(id = R.string.skippable_trait_dismiss),
-                    tint = Color(color = 0xFFE3E9F1)
                 )
             }
         }
