afc7cc344aa7bf0ccbb90a6dc5b45a59b39f9cd2
Andre Tortolano <andret@appcues.com>
2022-03-21 16:31:34 -0300

    ♻️  Inclusion of ContentHolderTrait

--- appcues/src/main/java/com/appcues/trait/TraitRegistry.kt
+++ appcues/src/main/java/com/appcues/trait/TraitRegistry.kt
@@ -3,6 +3,7 @@ package com.appcues.trait
 import com.appcues.data.model.AppcuesConfigMap
 import com.appcues.logging.Logcues
 import com.appcues.trait.appcues.BackdropTrait
+import com.appcues.trait.appcues.CarouselTrait
 import com.appcues.trait.appcues.ModalTrait
 import com.appcues.trait.appcues.SkippableTrait
 import org.koin.core.component.KoinScopeComponent
@@ -24,6 +25,7 @@ internal class TraitRegistry(
         register("@appcues/backdrop") { get<BackdropTrait> { parametersOf(it) } }
         register("@appcues/modal") { get<ModalTrait> { parametersOf(it) } }
         register("@appcues/skippable") { get<SkippableTrait> { parametersOf(it) } }
+        register("@appcues/carousel") { get<CarouselTrait> { parametersOf(it) } }
     }
 
     operator fun get(key: String): TraitFactoryBlock? {
