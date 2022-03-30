bc037c45bae866b9d9f6ca1a0b4c4934d099bc03
James Ellis <james@appcues.com>
2022-03-30 16:41:12 -0400

    👌 Add comments, placeholder QualifyResponseDeserializer

--- appcues/src/main/java/com/appcues/data/GsonConfiguration.kt
+++ appcues/src/main/java/com/appcues/data/GsonConfiguration.kt
@@ -1,6 +1,8 @@
 package com.appcues.data
 
+import com.appcues.data.remote.response.QualifyResponse
 import com.appcues.data.remote.response.step.StepContainerResponse
+import com.appcues.data.remote.retrofit.deserializer.QualifyResponseDeserializer
 import com.appcues.data.remote.retrofit.deserializer.StepContainerResponseDeserializer
 import com.appcues.data.remote.retrofit.serializer.DateSerializer
 import com.google.gson.Gson
@@ -10,6 +12,7 @@ import java.util.Date
 internal object GsonConfiguration {
     fun getGson(): Gson = GsonBuilder()
         .registerTypeAdapter(Date::class.java, DateSerializer())
+        .registerTypeAdapter(QualifyResponse::class.java, QualifyResponseDeserializer())
         .registerTypeAdapter(StepContainerResponse::class.java, StepContainerResponseDeserializer())
         .create()
 }
