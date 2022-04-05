f3551925cf106b472b8a35f80492506468858fbb
James Ellis <james@appcues.com>
2022-04-04 20:00:23 -0400

    ♻️ Update state machine for improved handling of errors and continuations

--- appcues/src/main/java/com/appcues/statemachine/Transition.kt
+++ appcues/src/main/java/com/appcues/statemachine/Transition.kt
@@ -1,41 +1,11 @@
 package com.appcues.statemachine
 
-import com.appcues.statemachine.SideEffect.ContinuationEffect
-import com.appcues.statemachine.SideEffect.PresentContainerEffect
 import com.appcues.statemachine.SideEffect.ReportErrorEffect
 
 internal open class Transition(
-    private val state: State?,
-    private val sideEffect: SideEffect? = null
+    val state: State?,
+    val sideEffect: SideEffect? = null
 ) {
-
-    suspend fun applyTransition(stateMachine: StateMachine, stateBlock: suspend (State) -> Unit) {
-        if (state != null) {
-            stateBlock(state)
-        }
-
-        if (sideEffect != null) {
-            when (sideEffect) {
-                is ContinuationEffect -> sideEffect.applyEffect(stateMachine)
-                is ReportErrorEffect -> sideEffect.applyEffect(stateMachine)
-                is PresentContainerEffect -> sideEffect.applyEffect()
-            }
-        }
-    }
-
-    private fun ContinuationEffect.applyEffect(stateMachine: StateMachine) {
-        stateMachine.handleAction(action)
-    }
-
-    private suspend fun ReportErrorEffect.applyEffect(stateMachine: StateMachine) {
-        stateMachine.reportError(error)
-    }
-
-    private fun PresentContainerEffect.applyEffect() {
-        experience.stepContainers[containerIndex].presentingTrait.present()
-    }
-
     class ErrorLoggingTransition(error: Error) : Transition(null, ReportErrorEffect(error))
-
     class EmptyTransition : Transition(null, null)
 }
