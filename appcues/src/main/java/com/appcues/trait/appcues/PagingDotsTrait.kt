70662b7cf2b002b9cd44676c72c807db743697b1
James Ellis <james@appcues.com>
2022-04-13 15:36:05 -0400

    ✨ Add support for margin values on paging-dots trait

--- appcues/src/main/java/com/appcues/trait/appcues/PagingDotsTrait.kt
+++ appcues/src/main/java/com/appcues/trait/appcues/PagingDotsTrait.kt
@@ -35,6 +35,7 @@ internal class PagingDotsTrait(
 
     companion object {
         const val TYPE = "@appcues/paging-dots"
+        private const val DEFAULT_PADDING = 8
     }
 
     private val style = config.getConfigStyle("style")
@@ -61,6 +62,12 @@ internal class PagingDotsTrait(
         Box(
             modifier = Modifier
                 .align(style.getBoxAlignment())
+                .padding(
+                    (DEFAULT_PADDING + (style?.marginLeading ?: 0)).dp,
+                    (DEFAULT_PADDING + (style?.marginTop ?: 0)).dp,
+                    (DEFAULT_PADDING + (style?.marginTrailing ?: 0)).dp,
+                    (DEFAULT_PADDING + (style?.marginBottom ?: 0)).dp
+                )
                 .padding(8.dp),
             contentAlignment = Alignment.CenterStart
         ) {
