941e933f01b5fa4ea7aed1f6e827d3c6e50fbe01
Andre Tortolano <andret@appcues.com>
2022-04-27 17:11:17 -0300

    👌 Adjustments from code review #1

--- appcues/src/main/java/com/appcues/debugger/ui/details/DebuggerEventDetails.kt
+++ appcues/src/main/java/com/appcues/debugger/ui/details/DebuggerEventDetails.kt
@@ -20,8 +20,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
