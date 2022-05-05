1278b6b322b3eb24636d8a82224d7b5655ecae24
Andre Tortolano <andret@appcues.com>
2022-05-05 16:25:03 -0300

    🐛 Fixed bug with carousel content size one step ahead

--- appcues/src/main/java/com/appcues/trait/appcues/CarouselTrait.kt
+++ appcues/src/main/java/com/appcues/trait/appcues/CarouselTrait.kt
@@ -1,18 +1,24 @@
 package com.appcues.trait.appcues
 
-import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import com.appcues.trait.ContentHolderTrait
 import com.appcues.trait.ContentHolderTrait.ContainerPages
 import com.appcues.ui.AppcuesPaginationData
 import com.appcues.ui.LocalAppcuesPaginationDelegate
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.rememberPagerState
 
 internal class CarouselTrait(
@@ -50,11 +56,49 @@ internal class CarouselTrait(
                 .collect { localPagination.onPageChanged(it) }
         }
 
+        val horizontalPagerSize = remember { HorizontalPagerSize(pagerState) }
+
         HorizontalPager(
-            modifier = Modifier.animateContentSize(),
+            modifier = Modifier.size(with(LocalDensity.current) { horizontalPagerSize.pagerContainerSize.toDp() }),
             count = containerPages.pageCount,
             state = pagerState,
             verticalAlignment = Alignment.Top
-        ) { index -> containerPages.composePage(index) }
+        ) { index ->
+            Box(
+                // gets the size of each page and store it in a remembered state
+                modifier = Modifier.onSizeChanged { horizontalPagerSize.onHeightChanged(index, it.height) }
+            ) {
+                containerPages.composePage(index)
+            }
+        }
+    }
+
+    @ExperimentalPagerApi
+    private class HorizontalPagerSize constructor(private val pagerState: PagerState) {
+
+        val heightMap = mutableStateMapOf<Int, Int>()
+
+        val pagerContainerSize
+            get() = run {
+                val currentPageSize = heightMap[pagerState.currentPage] ?: 0
+                val nextPageSize = when {
+                    pagerState.currentPageOffset > 0 -> heightMap[pagerState.currentPage + 1] ?: 0
+                    pagerState.currentPageOffset < 0 -> heightMap[pagerState.currentPage - 1] ?: 0
+                    else -> currentPageSize
+                }
+
+                val offsetNormalized = if (pagerState.currentPageOffset < 0)
+                    pagerState.currentPageOffset * -1
+                else
+                    pagerState.currentPageOffset
+
+                (nextPageSize - currentPageSize) * offsetNormalized + currentPageSize
+            }
+
+        fun onHeightChanged(index: Int, height: Int) {
+            if (height != 0 && heightMap[index] ?: 0 == 0) {
+                heightMap[index] = height
+            }
+        }
     }
 }
