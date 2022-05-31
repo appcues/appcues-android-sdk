47deb092bbfed4ed9090875553722ed11d14c59b
Andre Tortolano <andret@appcues.com>
2022-05-31 15:39:32 -0300

    ♻️  Ignoring possible phantom properties from the json response

--- appcues/src/main/java/com/appcues/ui/modal/SharedModal.kt
+++ appcues/src/main/java/com/appcues/ui/modal/SharedModal.kt
@@ -10,12 +10,15 @@ import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.appcues.data.model.styling.ComponentStyle
 import com.appcues.ui.extensions.WindowInfo
 import com.appcues.ui.extensions.WindowInfo.DeviceType.MOBILE
 import com.appcues.ui.extensions.WindowInfo.DeviceType.TABLET
 import com.appcues.ui.extensions.WindowInfo.Orientation.PORTRAIT
+import com.appcues.ui.extensions.coloredShadow
+import com.appcues.ui.extensions.styleBorder
 import com.appcues.ui.extensions.styleShadow
 
 @OptIn(ExperimentalAnimationApi::class)
@@ -32,24 +35,39 @@ internal fun dialogExitTransition(): ExitTransition {
 internal fun Modifier.dialogModifier(style: ComponentStyle?, isDark: Boolean) =
     then(Modifier.styleShadow(style, isDark))
         .then(
-            if (style?.cornerRadius != null && style.cornerRadius != 0) Modifier
-                .clip(RoundedCornerShape(style.cornerRadius.dp))
-            else Modifier
+            if (style?.cornerRadius != null && style.cornerRadius != 0)
+                Modifier
+                    .clip(RoundedCornerShape(style.cornerRadius.dp))
+                    .styleBorder(style, isDark)
+            else
+                Modifier
         )
 
-internal fun Modifier.sheetModifier(windowInfo: WindowInfo, style: ComponentStyle?, isDark: Boolean) = then(
+internal fun Modifier.sheetModifier(windowInfo: WindowInfo, style: ComponentStyle?) = then(
     when (windowInfo.deviceType) {
         MOBILE -> if (windowInfo.orientation == PORTRAIT && style?.cornerRadius != null && style.cornerRadius != 0)
             Modifier.clip(RoundedCornerShape(topStart = style.cornerRadius.dp, topEnd = style.cornerRadius.dp))
         else
             Modifier
-        TABLET -> dialogModifier(style, isDark)
+        TABLET ->
+            Modifier
+                .coloredShadow(
+                    color = Color(color = 0xEE777777),
+                    radius = 12.dp
+                )
+                .clip(RoundedCornerShape(12.dp))
     }
 )
 
-internal fun Modifier.fullModifier(windowInfo: WindowInfo, style: ComponentStyle?, isDark: Boolean) = then(
+internal fun Modifier.fullModifier(windowInfo: WindowInfo) = then(
     when (windowInfo.deviceType) {
         MOBILE -> Modifier
-        TABLET -> dialogModifier(style, isDark)
+        TABLET ->
+            Modifier
+                .coloredShadow(
+                    color = Color(color = 0xEE777777),
+                    radius = 12.dp
+                )
+                .clip(RoundedCornerShape(12.dp))
     }
 )
