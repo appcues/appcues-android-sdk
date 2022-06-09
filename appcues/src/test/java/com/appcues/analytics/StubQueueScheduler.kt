bc7f61a9056516e6529d3a7708810a768f795bf1
Andre Tortolano <andret@appcues.com>
2022-06-09 17:11:33 -0300

    👌 Added more test to analytics

---
+++ appcues/src/test/java/com/appcues/analytics/StubQueueScheduler.kt
@@ -0,0 +1,18 @@
+package com.appcues.analytics
+
+import com.appcues.analytics.AnalyticsQueueProcessor.QueueScheduler
+import io.mockk.mockk
+
+internal class StubQueueScheduler : QueueScheduler {
+
+    val mockkScheduler: QueueScheduler = mockk(relaxed = true)
+
+    override fun schedule(block: () -> Unit) {
+        mockkScheduler.schedule(block)
+        block()
+    }
+
+    override fun cancel() {
+        mockkScheduler.cancel()
+    }
+}
