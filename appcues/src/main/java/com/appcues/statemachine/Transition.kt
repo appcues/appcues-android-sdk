147fb39a06c2fd33f9234a0acfd5aa2dc9b6a335
James Ellis <james@appcues.com>
2022-08-02 21:37:54 -0400

    🐛 Fix analytics reporting during activity pause/resume

--- appcues/src/main/java/com/appcues/statemachine/Transition.kt
+++ appcues/src/main/java/com/appcues/statemachine/Transition.kt
@@ -4,7 +4,8 @@ import com.appcues.statemachine.SideEffect.ReportErrorEffect
 
 internal open class Transition(
     val state: State?,
-    val sideEffect: SideEffect? = null
+    val sideEffect: SideEffect? = null,
+    val emitStateChange: Boolean = true,
 ) {
     class ErrorLoggingTransition(error: Error) : Transition(null, ReportErrorEffect(error))
     class EmptyTransition : Transition(null, null)
