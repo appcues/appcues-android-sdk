c035c8e02f38a79e1b8b0be48d2f18168a66ce54
Andre Tortolano <andret@appcues.com>
2022-08-03 10:51:24 -0400

    🐛 Moved all read/put from main to io thread on Storage

--- appcues/src/main/java/com/appcues/Storage.kt
+++ appcues/src/main/java/com/appcues/Storage.kt
@@ -2,6 +2,7 @@ package com.appcues
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.StrictMode
 import java.util.Date
 import java.util.UUID
 
@@ -9,6 +10,7 @@ internal class Storage(
     context: Context,
     config: AppcuesConfig
 ) {
+
     private enum class Constants(val rawVal: String) {
         DeviceId("appcues.deviceId"),
         UserId("appcues.userId"),
@@ -17,27 +19,32 @@ internal class Storage(
         LastContentShownAt("appcues.lastContentShownAt")
     }
 
-    private val sharedPreferences: SharedPreferences =
+    private val sharedPreferences: SharedPreferences = allowReads {
+        // this is also considered a read from disk
         context.getSharedPreferences("com.appcues.storage.${config.applicationId}", Context.MODE_PRIVATE)
+    }
 
     var deviceId: String
-        get() = sharedPreferences.getString(Constants.DeviceId.rawVal, null) ?: ""
+        get() = allowReads { sharedPreferences.getString(Constants.DeviceId.rawVal, null) ?: "" }
         set(value) = sharedPreferences.edit().putString(Constants.DeviceId.rawVal, value).apply()
 
     var userId: String
-        get() = sharedPreferences.getString(Constants.UserId.rawVal, null) ?: ""
+        get() = allowReads { sharedPreferences.getString(Constants.UserId.rawVal, null) ?: "" }
         set(value) = sharedPreferences.edit().putString(Constants.UserId.rawVal, value).apply()
 
     var groupId: String?
-        get() = sharedPreferences.getString(Constants.GroupId.rawVal, null)
+        get() = allowReads { sharedPreferences.getString(Constants.GroupId.rawVal, null) }
         set(value) = sharedPreferences.edit().putString(Constants.GroupId.rawVal, value).apply()
 
     var isAnonymous: Boolean
-        get() = sharedPreferences.getBoolean(Constants.IsAnonymous.rawVal, true)
+        get() = allowReads { sharedPreferences.getBoolean(Constants.IsAnonymous.rawVal, true) }
         set(value) = sharedPreferences.edit().putBoolean(Constants.IsAnonymous.rawVal, value).apply()
 
     var lastContentShownAt: Date?
-        get() = sharedPreferences.getLong(Constants.LastContentShownAt.rawVal, 0).run { if (this > 0) Date(this) else null }
+        get() = allowReads {
+            sharedPreferences.getLong(Constants.LastContentShownAt.rawVal, 0)
+                .run { if (this > 0) Date(this) else null }
+        }
         set(value) = when (value) {
             null -> sharedPreferences.edit().remove(Constants.LastContentShownAt.rawVal).apply()
             else -> sharedPreferences.edit().putLong(Constants.LastContentShownAt.rawVal, value.time).apply()
@@ -46,6 +53,15 @@ internal class Storage(
     init {
         if (deviceId.isEmpty()) {
             deviceId = UUID.randomUUID().toString()
+        }
+    }
+
+    private fun <T> allowReads(block: () -> T): T {
+        val oldPolicy = StrictMode.allowThreadDiskReads()
+        try {
+            return block()
+        } finally {
+            StrictMode.setThreadPolicy(oldPolicy)
         }
     }
 }
--- samples/kotlin-android-app/src/main/java/com/appcues/samples/kotlin/ExampleApplication.kt
+++ samples/kotlin-android-app/src/main/java/com/appcues/samples/kotlin/ExampleApplication.kt
@@ -1,6 +1,9 @@
 package com.appcues.samples.kotlin
 
 import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
 import com.appcues.Appcues
 import com.appcues.LoggingLevel
 
@@ -14,6 +17,25 @@ class ExampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .build()
+            )
+
+            StrictMode.setVmPolicy(
+                VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .build()
+            )
+        }
 
         appcues = Appcues(this, BuildConfig.APPCUES_ACCOUNT_ID, BuildConfig.APPCUES_APPLICATION_ID) {
             loggingLevel = if (BuildConfig.DEBUG) LoggingLevel.DEBUG else LoggingLevel.INFO
