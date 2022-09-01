f851eceec222c3eef4c334df6ca285d728e16dd0
James Ellis <james@appcues.com>
2022-09-01 16:37:01 -0400

    ♻️ Revise the UI for form input controls from review/feedback

--- appcues/src/main/java/com/appcues/ui/primitive/TextInputPrimitive.kt
+++ appcues/src/main/java/com/appcues/ui/primitive/TextInputPrimitive.kt
@@ -2,18 +2,23 @@ package com.appcues.ui.primitive
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.appcues.data.model.ExperiencePrimitive.TextInputPrimitive
 import com.appcues.data.model.ExperiencePrimitive.TextPrimitive
 import com.appcues.data.model.styling.ComponentColor
@@ -27,6 +32,7 @@ import com.appcues.data.model.styling.ComponentDataType.TEXT
 import com.appcues.ui.ExperienceStepFormItem.SingleTextFormItem
 import com.appcues.ui.LocalExperienceStepFormStateDelegate
 import com.appcues.ui.extensions.applyStyle
+import com.appcues.ui.extensions.getHorizontalAlignment
 import com.appcues.ui.theme.AppcuesPreviewPrimitive
 import java.util.UUID
 
@@ -40,33 +46,44 @@ internal fun TextInputPrimitive.Compose(modifier: Modifier) {
         formState.captureFormItem(this@Compose.id, SingleTextFormItem(label.text, required, text.value))
     }
 
-    // TBD what this should actually look like in our product, and how builder
-    // styling options will apply.
-    // Several customization options for TextField noted here https://stackoverflow.com/a/68592613
-    TextField(
-        value = text.value,
-        onValueChange = {
-            if (maxLength == null || it.length <= maxLength) {
-                text.value = it
-            }
-        },
-        modifier = modifier,
-        label = { label.Compose(modifier = Modifier) },
-        textStyle = LocalTextStyle.current.applyStyle(
-            style = textFieldStyle,
-            context = LocalContext.current,
-            isDark = isSystemInDarkTheme(),
-        ),
-        maxLines = numberOfLines,
-        singleLine = numberOfLines == 1,
-        placeholder = placeholder?.let {
-            // future plan is to replace this with a text primitive directly
-            {
-                Text(text = it)
-            }
-        },
-        keyboardOptions = KeyboardOptions(keyboardType = mapKeyboardType(dataType)),
-    )
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = style.getHorizontalAlignment(),
+    ) {
+        label.Compose()
+
+        // Several styling customization options for TextField noted here https://stackoverflow.com/a/68592613
+        OutlinedTextField(
+            value = text.value,
+            onValueChange = {
+                if (maxLength == null || it.length <= maxLength) {
+                    text.value = it
+                }
+            },
+            modifier = Modifier.fillMaxWidth(),
+            textStyle = LocalTextStyle.current.applyStyle(
+                style = textFieldStyle,
+                context = LocalContext.current,
+                isDark = isSystemInDarkTheme(),
+            ),
+            shape = RoundedCornerShape(8.dp),
+            maxLines = numberOfLines,
+            singleLine = numberOfLines == 1,
+            placeholder = placeholder?.let {
+                // future plan is to replace this with a text primitive directly
+                {
+                    Text(text = it)
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = mapKeyboardType(dataType)),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent,
+                cursorColor = Color.Black,
+                focusedIndicatorColor = Color.LightGray,
+                unfocusedIndicatorColor = Color.LightGray
+            ),
+        )
+    }
 }
 
 private fun mapKeyboardType(value: ComponentDataType): KeyboardType = when (value) {
