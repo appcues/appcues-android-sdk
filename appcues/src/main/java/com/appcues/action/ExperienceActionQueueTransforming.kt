a75e05c1114b85f4d4f215ce40774dd2cbbc9abd
James Ellis <james@appcues.com>
2022-10-03 17:10:40 -0400

    ✨ Add ExperienceActionQueueTransforming and link into the ActionProcessor handling

---
+++ appcues/src/main/java/com/appcues/action/ExperienceActionQueueTransforming.kt
@@ -0,0 +1,20 @@
+package com.appcues.action
+
+import com.appcues.Appcues
+
+/**
+ * An `ExperienceAction` that performs modifications of the action queue executed following an interaction in an experience.
+ */
+interface ExperienceActionQueueTransforming : ExperienceAction {
+
+    /**
+     *  Modify the queue of actions executed in an experience.
+     *
+     * @param queue The current queue of actions.
+     * @param index The index of the current action in the `queue`.
+     * @param appcues The `Appcues` instance that displayed the experience triggering the action.
+     *
+     * @return The updated queue.
+     */
+    fun transformQueue(queue: List<ExperienceAction>, index: Int, appcues: Appcues): List<ExperienceAction>
+}
