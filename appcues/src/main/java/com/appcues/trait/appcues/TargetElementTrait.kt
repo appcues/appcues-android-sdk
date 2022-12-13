50d2d6565a7bc16851ce6b1ace54466130582772
Andre Tortolano <andret@appcues.com>
2022-12-13 11:11:03 -0500

    👌 adjustments from code review

--- appcues/src/main/java/com/appcues/trait/appcues/TargetElementTrait.kt
+++ appcues/src/main/java/com/appcues/trait/appcues/TargetElementTrait.kt
@@ -1,5 +1,8 @@
 package com.appcues.trait.appcues
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import com.appcues.data.model.AppcuesConfigMap
 import com.appcues.data.model.getConfigInt
 import com.appcues.trait.MetadataSettingTrait
@@ -15,20 +18,17 @@ internal class TargetElementTrait(
         const val METADATA_TARGET_RECT = "targetRectangle"
     }
 
-    data class TargetRect(
-        val x: Int,
-        val y: Int,
-        val width: Int,
-        val height: Int,
-    )
-
     override fun produceMetadata(): Map<String, Any?> {
         // TODO replace this with dynamic element found based on "selector" config property
-        val rect = TargetRect(
-            x = config.getConfigInt("x") ?: 0,
-            y = config.getConfigInt("y") ?: 0,
-            width = config.getConfigInt("width") ?: 0,
-            height = config.getConfigInt("height") ?: 0
+        val rect = Rect(
+            Offset(
+                x = config.getConfigInt("x")?.toFloat() ?: 0f,
+                y = config.getConfigInt("y")?.toFloat() ?: 0f
+            ),
+            Size(
+                width = config.getConfigInt("width")?.toFloat() ?: 0f,
+                height = config.getConfigInt("height")?.toFloat() ?: 0f
+            )
         )
 
         return hashMapOf(METADATA_TARGET_RECT to rect)
