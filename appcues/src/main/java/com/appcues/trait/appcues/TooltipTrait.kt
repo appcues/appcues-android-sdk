f9fdb69e93ec795ff05b6c6bfa9a8fbe1394c834
Andre Tortolano <andret@appcues.com>
2023-01-05 11:22:01 -0500

    👌 fixed a problem with tooltip positioning and applied backgroundColor

--- appcues/src/main/java/com/appcues/trait/appcues/TooltipTrait.kt
+++ appcues/src/main/java/com/appcues/trait/appcues/TooltipTrait.kt
@@ -1,7 +1,7 @@
 package com.appcues.trait.appcues
 
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -15,23 +15,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
 import com.appcues.data.model.AppcuesConfigMap
+import com.appcues.data.model.getConfigStyle
 import com.appcues.trait.AppcuesTraitAnimatedVisibility
 import com.appcues.trait.ContentWrappingTrait
 import com.appcues.trait.PresentingTrait
+import com.appcues.trait.appcues.TooltipTrait.PointerPosition.BOTTOM
+import com.appcues.trait.appcues.TooltipTrait.PointerPosition.NONE
+import com.appcues.trait.appcues.TooltipTrait.PointerPosition.TOP
 import com.appcues.ui.AppcuesOverlayViewManager
 import com.appcues.ui.composables.LocalAppcuesStepMetadata
 import com.appcues.ui.composables.rememberAppcuesContentVisibility
+import com.appcues.ui.extensions.styleBackground
 import com.appcues.ui.modal.dialogEnterTransition
 import com.appcues.ui.modal.dialogExitTransition
 import com.appcues.ui.utils.AppcuesWindowInfo
@@ -52,6 +56,16 @@ internal class TooltipTrait(
         private val MAX_HEIGHT_DP = 200.dp
     }
 
+    private enum class PointerPosition {
+        TOP, BOTTOM, NONE
+    }
+
+    private val style = config.getConfigStyle("style")
+
+    override fun present() {
+        AppcuesOverlayViewManager(scope = scope).addView()
+    }
+
     @Composable
     override fun WrapContent(content: @Composable (hasFixedHeight: Boolean, contentPadding: PaddingValues?) -> Unit) {
         val windowInfo = rememberAppcuesWindowInfo()
@@ -63,12 +77,15 @@ internal class TooltipTrait(
             (metadata.actual[TargetElementTrait.METADATA_TARGET_RECT] as Rect?)
         }
 
-        val rectTopOfScreen = remember(metadata) {
-            // true if rect position is located before center height of the screen
-            if (actualRect != null) actualRect.center.y.dp < windowInfo.heightDp / 2 else false
+        val pointerPosition = remember(metadata) {
+            when {
+                actualRect == null -> PointerPosition.NONE
+                actualRect.center.y.dp < windowInfo.heightDp / 2 -> PointerPosition.TOP
+                else -> PointerPosition.BOTTOM
+            }
         }
 
-        val tooltipSize = remember { mutableStateOf(IntSize(0, 0)) }
+        val tooltipSize = remember { mutableStateOf(DpSize(0.dp, 0.dp)) }
 
         AppcuesTraitAnimatedVisibility(
             visibleState = rememberAppcuesContentVisibility(),
@@ -79,34 +96,34 @@ internal class TooltipTrait(
                 Column(
                     modifier = Modifier
                         .padding(SCREEN_PADDING)
-                        .positionTooltip(actualRect, tooltipSize.value, rectTopOfScreen, density, windowInfo)
+                        .positionTooltip(actualRect, tooltipSize.value, pointerPosition, windowInfo)
                 ) {
-                    if (rectTopOfScreen) {
+                    if (pointerPosition == TOP) {
                         Box(
                             modifier = Modifier
                                 .padding(start = 32.dp)
                                 .size(20.dp)
                                 .clip(triangleShape)
-                                .background(Color.Green)
+                                .styleBackground(style, isSystemInDarkTheme())
                         )
                     }
                     Box(
                         modifier = Modifier
                             .requiredSizeIn(maxWidth = MAX_WIDTH_DP, maxHeight = MAX_HEIGHT_DP)
-                            .onSizeChanged { tooltipSize.value = it }
+                            .onSizeChanged { with(density) { tooltipSize.value = DpSize(it.width.toDp(), it.height.toDp()) } }
                             .clip(RoundedCornerShape(8.dp))
-                            .background(Color.Red)
+                            .styleBackground(style, isSystemInDarkTheme())
                     ) {
                         content(false, PaddingValues(0.dp))
                     }
-                    if (!rectTopOfScreen) {
+                    if (pointerPosition == BOTTOM) {
                         Box(
                             modifier = Modifier
                                 .padding(start = 32.dp)
                                 .size(20.dp)
                                 .rotate(degrees = 180f)
                                 .clip(triangleShape)
-                                .background(Color.Green)
+                                .styleBackground(style, isSystemInDarkTheme())
                         )
                     }
                 }
@@ -115,52 +132,33 @@ internal class TooltipTrait(
     }
 
     private val triangleShape = GenericShape { size, _ ->
-        // 1)
         moveTo(size.width / 2f, 0f)
-
-        // 2)
         lineTo(size.width, size.height)
-
-        // 3)
         lineTo(0f, size.height)
     }
 
-    override fun present() {
-        AppcuesOverlayViewManager(scope = scope).addView()
-    }
-
-    @Composable
     private fun Modifier.positionTooltip(
         actualRect: Rect?,
-        tooltipSize: IntSize,
-        rectTopOfScreen: Boolean,
-        density: Density,
+        tooltipSize: DpSize,
+        pointerPosition: PointerPosition,
         windowInfo: AppcuesWindowInfo
-    ): Modifier {
+    ): Modifier = composed {
         val tooltipPaddingTop = animateDpAsState(
-            targetValue = with(density) {
-                if (actualRect != null) {
-                    if (rectTopOfScreen) {
-                        max((actualRect.bottom.dp), 0.dp)
-                    } else {
-                        max((actualRect.top.dp - tooltipSize.height.dp - SCREEN_PADDING - 20.dp), 0.dp)
-                    }
-                } else {
-                    windowInfo.heightDp - tooltipSize.height.dp - SCREEN_PADDING
-                }
+            targetValue = when (pointerPosition) {
+                TOP -> max((actualRect?.let { it.bottom.dp } ?: 0.dp), 0.dp)
+                BOTTOM -> max((actualRect?.let { it.top.dp - tooltipSize.height - SCREEN_PADDING - 20.dp } ?: 0.dp), 0.dp)
+                NONE -> windowInfo.heightDp - tooltipSize.height - SCREEN_PADDING
             }
         )
 
         val toolTipPaddingStart = animateDpAsState(
-            targetValue = with(density) {
-                if (actualRect != null) {
-                    max(0.dp, 0.dp)
-                } else {
-                    max((windowInfo.widthDp - (SCREEN_PADDING * 2) - tooltipSize.width.dp) / 2, 0.dp)
-                }
+            targetValue = if (actualRect != null) {
+                max(0.dp, 0.dp)
+            } else {
+                max((windowInfo.widthDp - (SCREEN_PADDING * 2) - tooltipSize.width) / 2, 0.dp)
             }
         )
 
-        return then(Modifier.padding(start = toolTipPaddingStart.value, top = tooltipPaddingTop.value))
+        then(Modifier.padding(start = toolTipPaddingStart.value, top = tooltipPaddingTop.value))
     }
 }
