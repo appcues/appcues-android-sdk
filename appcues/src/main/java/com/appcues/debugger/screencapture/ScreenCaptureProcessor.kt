5a96ca10a513d8926feebe812944565e691fc9f0
James Ellis <james@appcues.com>
2023-02-24 09:47:58 -0500

    👌 Extract ScreenCaptureProcessor to centralize responsibilities around creating and saving screen captures

--- appcues/src/main/java/com/appcues/debugger/screencapture/ScreenCaptureProcessor.kt
+++ appcues/src/main/java/com/appcues/debugger/screencapture/ScreenCaptureProcessor.kt
@@ -7,12 +7,72 @@ import android.text.format.DateFormat
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.children
+import com.appcues.AppcuesConfig
 import com.appcues.R
 import com.appcues.monitor.AppcuesActivityMonitor
 import com.appcues.ui.ElementSelector
+import com.appcues.util.ContextResources
+import com.appcues.util.ResultOf
+import com.appcues.util.ResultOf.Success
 import java.util.Date
 
-internal fun View.screenCaptureDisplayName(timestamp: Date): String {
+internal class ScreenCaptureProcessor(
+    private val config: AppcuesConfig,
+    private val contextResources: ContextResources,
+) {
+
+    fun captureScreen(): Capture? {
+        return AppcuesActivityMonitor.activity?.window?.decorView?.rootView?.let {
+            prepare(it)
+
+            val timestamp = Date()
+            val displayName = it.screenCaptureDisplayName(timestamp)
+            val screenshot = it.screenshot()
+            val layout = it.asCaptureView()
+            val capture = if (screenshot != null && layout != null) {
+                Capture(
+                    appId = config.applicationId,
+                    displayName = displayName,
+                    screenshotImageUrl = null,
+                    layout = layout,
+                    metadata = contextResources.generateCaptureMetadata(),
+                    timestamp = timestamp,
+                ).apply {
+                    this.screenshot = screenshot
+                }
+            } else null
+
+            restore(it)
+
+            capture
+        }
+    }
+
+    suspend fun save(capture: Capture): ResultOf<Capture, Error> {
+
+        // upcoming work to execute API calls starts here
+
+        return Success(capture)
+    }
+
+    private fun prepare(view: View) {
+        // hide the debugger view for screen capture, if present
+        val debuggerView = view.findViewById<View>(R.id.appcues_debugger_view)
+        debuggerView?.let { it ->
+            it.visibility = View.GONE
+        }
+    }
+
+    private fun restore(view: View) {
+        // hide the debugger view for screen capture, if present
+        val debuggerView = view.findViewById<View>(R.id.appcues_debugger_view)
+        debuggerView?.let { it ->
+            it.visibility = View.VISIBLE
+        }
+    }
+}
+
+private fun View.screenCaptureDisplayName(timestamp: Date): String {
     var name: String
     val activity = AppcuesActivityMonitor.activity
     if (activity != null) {
@@ -26,7 +86,7 @@ internal fun View.screenCaptureDisplayName(timestamp: Date): String {
     return "$name (${DateFormat.format("yyyy-MM-dd_HH:mm:ss", timestamp)})"
 }
 
-internal fun View.asCaptureView(): Capture.View? {
+private fun View.asCaptureView(): Capture.View? {
     val displayMetrics = context.resources.displayMetrics
     val density = displayMetrics.density
 
@@ -78,7 +138,7 @@ internal fun View.asCaptureView(): Capture.View? {
     )
 }
 
-internal fun View.screenshot() =
+private fun View.screenshot() =
     if (this.width > 0 && this.height > 0) {
         val bitmap = Bitmap.createBitmap(this.width, this.height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(bitmap)
