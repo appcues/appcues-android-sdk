c9d76aeaf18812ce427a2786aadb425b253f69d2
James Ellis <james@appcues.com>
2023-03-09 10:56:14 -0500

    ✨ Add @appcues/request-review action

---
+++ appcues/src/main/java/com/appcues/action/appcues/RequestReviewAction.kt
@@ -0,0 +1,30 @@
+package com.appcues.action.appcues
+
+import android.content.Context
+import com.appcues.Appcues
+import com.appcues.action.ExperienceAction
+import com.appcues.data.model.AppcuesConfigMap
+import com.appcues.ui.InAppReviewActivity
+import kotlinx.coroutines.CompletableDeferred
+import org.koin.core.scope.Scope
+
+internal class RequestReviewAction(
+    override val config: AppcuesConfigMap,
+    private val context: Context,
+    private val koinScope: Scope,
+) : ExperienceAction {
+
+    companion object {
+        const val TYPE = "@appcues/request-review"
+    }
+
+    override suspend fun execute(appcues: Appcues) {
+
+        val completion = CompletableDeferred<Boolean>()
+        InAppReviewActivity.completion = completion
+
+        context.startActivity(InAppReviewActivity.getIntent(context, koinScope.id))
+
+        completion.await()
+    }
+}
