0ef40a73360a0d30fc374f14a2a43b119028df05
James Ellis <james@appcues.com>
2023-03-22 16:30:05 -0400

    ♻️ Add weighted matching logic on target element selector handling

--- appcues/src/main/java/com/appcues/trait/appcues/TargetElementTrait.kt
+++ appcues/src/main/java/com/appcues/trait/appcues/TargetElementTrait.kt
@@ -5,6 +5,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.appcues.Appcues
 import com.appcues.ElementSelector
+import com.appcues.ViewElement
 import com.appcues.data.model.AppcuesConfigMap
 import com.appcues.data.model.getConfig
 import com.appcues.data.model.getConfigObject
@@ -28,15 +29,7 @@ internal class TargetElementTrait(
     private val preferredPosition = config.getConfig<String>("contentPreferredPosition").toPosition()
 
     override fun produceMetadata(): Map<String, Any?> {
-        // a null value here means it failed JSON deserialization
-        // see ElementSelectorAdapter for more details, but it basically means that there were
-        // no valid selector properties for the current ElementTargeting strategy in this app
-        if (selector == null) throw AppcuesTraitException("Invalid selector")
-
-        val matchingViews = Appcues.elementTargeting.findMatches(selector)
-
-        val selectedView = matchingViews.firstOrNull()
-            ?: throw AppcuesTraitException("could not find view matching selector ${selector.toMap()}")
+        val view = viewMatchingSelector()
 
         val rootView = AppcuesActivityMonitor.activity?.window?.decorView?.rootView
             ?: throw AppcuesTraitException("could not find root view")
@@ -50,14 +43,47 @@ internal class TargetElementTrait(
 
         // the selectedView position and size is already in Dp at this point, which is what we want in metadata
         val targetRectangle = TargetRectangleInfo(
-            x = (selectedView.x - insets.left.toDp(density)).toFloat(),
-            y = (selectedView.y - insets.top.toDp(density)).toFloat(),
-            width = selectedView.width.toFloat(),
-            height = selectedView.height.toFloat(),
+            x = (view.x - insets.left.toDp(density)).toFloat(),
+            y = (view.y - insets.top.toDp(density)).toFloat(),
+            width = view.width.toFloat(),
+            height = view.height.toFloat(),
             contentDistance = contentDistance,
             prefPosition = preferredPosition,
         )
 
         return hashMapOf(TARGET_RECTANGLE_METADATA to targetRectangle)
+    }
+
+    private fun viewMatchingSelector(): ViewElement {
+        // a null value here means it failed JSON deserialization
+        // see ElementSelectorAdapter for more details, but it basically means that there were
+        // no valid selector properties for the current ElementTargeting strategy in this app
+        if (selector == null) throw AppcuesTraitException("invalid selector")
+
+        val views = Appcues.elementTargeting.findMatches(selector)
+
+        if (views.isEmpty()) {
+            throw AppcuesTraitException("no view matching selector ${selector.toMap()}")
+        }
+
+        // if only a single match of anything, use it
+        if (views.count() == 1) {
+            return views[0]
+        }
+
+        // weight the selector property matches by how distinct they are considered
+        val weightedMatches = views.map {
+            val weight = it.selector?.evaluateMatch(selector) ?: 0
+            Pair(it, weight)
+        }
+
+        // find the max weight value from all matches
+        val maxWeight = weightedMatches.maxBy { it.second }.second
+        val maxItems = weightedMatches.filter { it.second == maxWeight }
+        if (maxItems.count() == 1) {
+            return maxItems[0].first
+        }
+
+        throw AppcuesTraitException("multiple non-distinct views (${views.count()}) matched selector ${selector.toMap()}")
     }
 }
