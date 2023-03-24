aaa9500b5df85ede34caef545dcbff3744bf4bfe
James Ellis <james@appcues.com>
2023-03-24 13:08:24 -0400

    ♻️ Update deprecated options menu usage to use new MenuProvider api

--- samples/kotlin-android-app/src/main/java/com/appcues/samples/kotlin/main/EventsFragment.kt
+++ samples/kotlin-android-app/src/main/java/com/appcues/samples/kotlin/main/EventsFragment.kt
@@ -7,7 +7,10 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import com.appcues.samples.kotlin.ExampleApplication
 import com.appcues.samples.kotlin.R
 import com.appcues.samples.kotlin.databinding.FragmentEventsBinding
@@ -21,11 +24,6 @@ class EventsFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val appcues = ExampleApplication.appcues
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,6 +43,30 @@ class EventsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.events_menu, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    return when (menuItem.itemId) {
+                        R.id.events_menu_debug -> {
+                            appcues.debug(requireActivity())
+                            true
+                        }
+                        else -> false
+                    }
+                }
+            },
+            viewLifecycleOwner,
+            Lifecycle.State.RESUMED
+        )
+    }
+
     override fun onResume() {
         super.onResume()
         appcues.screen("Trigger Events")
@@ -53,20 +75,5 @@ class EventsFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.events_menu, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.events_menu_debug -> {
-                appcues.debug(requireActivity())
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
