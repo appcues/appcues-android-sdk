69435329ee3895b03f2070cdd2b2eb668889f827
James Ellis <james@appcues.com>
2023-06-21 13:56:21 -0400

    👌 Add fallback logic to get top window and view for lower API levels

--- appcues/src/main/java/com/appcues/ui/AppcuesOverlayViewManager.kt
+++ appcues/src/main/java/com/appcues/ui/AppcuesOverlayViewManager.kt
@@ -27,6 +27,7 @@ import com.appcues.monitor.AppcuesActivityMonitor.ActivityMonitorListener
 import com.appcues.ui.composables.AppcuesComposition
 import com.appcues.ui.primitive.EmbedChromeClient
 import org.koin.core.scope.Scope
+import java.lang.reflect.Method
 
 internal class AppcuesOverlayViewManager(private val scope: Scope) : DefaultLifecycleObserver, ActivityMonitorListener {
 
@@ -140,33 +141,50 @@ internal class AppcuesOverlayViewManager(private val scope: Scope) : DefaultLife
 }
 
 internal fun Activity.getParentView(): ViewGroup {
-    // if there is any difference in API levels we can handle it here
-    if (VERSION.SDK_INT >= VERSION_CODES.Q) {
-        val views = WindowInspector.getGlobalWindowViews()
 
-        val decorView = views.last()
-        if (decorView != window.decorView) {
-            // This is the case of some other decorView showing on top - like a modal
-            // dialog. In this case, we need to apply the fixups below to ensure that our
-            // content can render correctly inside of this other view
-            if (ViewTreeLifecycleOwner.get(decorView) == null) {
-                val lifecycleOwner = window.decorView.findViewTreeLifecycleOwner()
-                ViewTreeLifecycleOwner.set(decorView, lifecycleOwner)
-            }
-
-            if (ViewTreeViewModelStoreOwner.get(decorView) == null) {
-                val viewModelStoreOwner = window.decorView.findViewTreeViewModelStoreOwner()
-                ViewTreeViewModelStoreOwner.set(decorView, viewModelStoreOwner)
-            }
-
-            if (decorView.findViewTreeSavedStateRegistryOwner() == null) {
-                val savedStateRegistryOwner = window.decorView.findViewTreeSavedStateRegistryOwner()
-                decorView.setViewTreeSavedStateRegistryOwner(savedStateRegistryOwner)
-            }
-        }
-
-        return decorView.rootView as ViewGroup
+    // try to find the most applicable decorView to inject Appcues content into. Typically there is just a single
+    // decorView on the Activity window. However, if something like a dialog modal has been shown, this can add another
+    // window with another decorView on top of the Activity. If we want to support showing content above that layer, we need
+    // to find the top most decorView like below.
+    
+    val decorView = if (VERSION.SDK_INT >= VERSION_CODES.Q) {
+        // this is the preferred method on API 29+ with the new WindowInspector function
+        WindowInspector.getGlobalWindowViews().last()
     } else {
-        return window.decorView.rootView as ViewGroup
+        @Suppress("SwallowedException", "TooGenericExceptionCaught")
+        try {
+            // this is the less desirable method for API 21-28, using reflection to try to get the root views
+            val windowManagerClass = Class.forName("android.view.WindowManagerGlobal")
+            val windowManager = windowManagerClass.getMethod("getInstance").invoke(null)
+            val getViewRootNames: Method = windowManagerClass.getMethod("getViewRootNames")
+            val getRootView: Method = windowManagerClass.getMethod("getRootView", String::class.java)
+            val rootViewNames = getViewRootNames.invoke(windowManager) as Array<Any?>
+            val rootViews = rootViewNames.map { getRootView(windowManager, it) as View }
+            rootViews.last()
+        } catch (e: Exception) {
+            // if all else fails, use the decorView on the window, which is typically the only one
+            window.decorView
+        }
     }
+
+    // This is the case of some other decorView showing on top - like a modal
+    // dialog. In this case, we need to apply the fix-ups below to ensure that our
+    // content can render correctly inside of this other view. In each case, we use
+    // the applicable value from the Activity default window.
+    if (ViewTreeLifecycleOwner.get(decorView) == null) {
+        val lifecycleOwner = window.decorView.findViewTreeLifecycleOwner()
+        ViewTreeLifecycleOwner.set(decorView, lifecycleOwner)
+    }
+
+    if (ViewTreeViewModelStoreOwner.get(decorView) == null) {
+        val viewModelStoreOwner = window.decorView.findViewTreeViewModelStoreOwner()
+        ViewTreeViewModelStoreOwner.set(decorView, viewModelStoreOwner)
+    }
+
+    if (decorView.findViewTreeSavedStateRegistryOwner() == null) {
+        val savedStateRegistryOwner = window.decorView.findViewTreeSavedStateRegistryOwner()
+        decorView.setViewTreeSavedStateRegistryOwner(savedStateRegistryOwner)
+    }
+
+    return decorView.rootView as ViewGroup
 }
