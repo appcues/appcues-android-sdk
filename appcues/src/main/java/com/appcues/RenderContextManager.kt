6cf667472436dd7e8fc6261177d3c60aff5d6ed2
andretortolano <andre@appcues.com>
2023-07-27 15:39:25 -0400

    ✨ Added AppcuesFrameView to replace generic ViewGroup

--- appcues/src/main/java/com/appcues/RenderContextManager.kt
+++ appcues/src/main/java/com/appcues/RenderContextManager.kt
@@ -1,19 +1,18 @@
 package com.appcues
 
-import android.view.ViewGroup
 import com.appcues.data.model.RenderContext
 import com.appcues.data.model.RenderContext.Embed
 import java.lang.ref.WeakReference
 
 internal class RenderContextManager {
 
-    private val slots: HashMap<RenderContext, WeakReference<ViewGroup>> = hashMapOf()
+    private val slots: HashMap<RenderContext, WeakReference<AppcuesFrameView>> = hashMapOf()
 
-    fun registerEmbedFrame(frameId: String, view: ViewGroup) {
-        slots[Embed(frameId)] = WeakReference(view)
+    fun registerEmbedFrame(frameId: String, frame: AppcuesFrameView) {
+        slots[Embed(frameId)] = WeakReference(frame)
     }
 
-    fun getEmbedView(renderContext: RenderContext): ViewGroup? {
+    fun getEmbedFrame(renderContext: RenderContext): AppcuesFrameView? {
         return when (renderContext) {
             is Embed -> slots[renderContext]?.get()
             else -> null
