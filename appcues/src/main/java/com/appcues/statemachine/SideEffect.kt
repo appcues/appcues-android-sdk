e79c916f88ea2fdd09aa5d95b8f7edffc4e67d8b
James Ellis <james@appcues.com>
2023-08-30 10:15:25 -0400

    🔥 Remove commented out code

--- appcues/src/main/java/com/appcues/statemachine/SideEffect.kt
+++ appcues/src/main/java/com/appcues/statemachine/SideEffect.kt
@@ -7,15 +7,7 @@ import kotlinx.coroutines.CompletableDeferred
 
 internal sealed class SideEffect {
     data class ContinuationEffect(val action: Action) : SideEffect()
-    data class PresentContainerEffect(
-        val presentContainer: suspend (ActionProcessor) -> Action
-//        val experience: Experience,
-//        val flatStepIndex: Int,
-//        val containerIndex: Int,
-//        val actions: List<ExperienceAction>,
-//        val produceMetadata: suspend () -> Unit,
-    ) : SideEffect()
-
+    data class PresentContainerEffect(val presentContainer: suspend (ActionProcessor) -> Action) : SideEffect()
     data class ReportErrorEffect(val error: Error) : SideEffect()
     data class AwaitEffect(val completion: CompletableDeferred<ResultOf<State, Error>>) : SideEffect()
     data class ProcessActions(val actions: List<ExperienceAction>) : SideEffect()
