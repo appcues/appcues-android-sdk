7acf82e0f63dc709c92116bd04b1d14a6fb2d841
James Ellis <james@appcues.com>
2023-09-25 15:46:58 -0400

    ✅ Add support for experience test snapshots

--- appcues/src/debug/java/com/appcues/CompositionTestHelper.kt
+++ appcues/src/debug/java/com/appcues/CompositionTestHelper.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import coil.ImageLoader
@@ -16,6 +17,7 @@ import com.appcues.data.mapper.experience.ExperienceMapper
 import com.appcues.data.mapper.step.primitives.mapPrimitive
 import com.appcues.data.model.ExperienceStepFormState
 import com.appcues.data.model.ExperienceTrigger
+import com.appcues.data.remote.appcues.response.QualifyResponse
 import com.appcues.data.remote.appcues.response.experience.ExperienceResponse
 import com.appcues.data.remote.appcues.response.step.primitive.PrimitiveResponse
 import com.appcues.data.remote.appcues.response.step.primitive.PrimitiveResponse.StackPrimitiveResponse
@@ -120,6 +122,76 @@ public fun ComposeContainer(context: Context, stepContentJson: List<String>?, tr
                 contentAlignment = Alignment.Center
             ) {
                 ComposeContainer(container, 0, metadata)
+            }
+        }
+    }
+}
+
+// This helper supports testing any experience JSON content, rendering the given group and step index.
+@Composable
+public fun ComposeContainer(
+    context: Context,
+    experienceJson: String,
+    groupIndex: Int,
+    stepIndex: Int,
+    animated: Boolean,
+    imageLoader: ImageLoader,
+) {
+    // set up a Koin scope for testing - for experience/trait mapping, trait registry, etc
+    val scope = AppcuesKoinContext.createAppcuesScope(context, AppcuesConfig("", ""))
+
+    // this JSON is actually a qualify response, and we'll get the first experience out of it
+    val qualifyResponse = MoshiConfiguration.moshi.adapter(QualifyResponse::class.java).fromJson(experienceJson)!!
+    val experienceResponse = qualifyResponse.experiences[0] as ExperienceResponse
+
+    // update the experience to strip out any actions, as they can rely on injected dependencies
+    // that are not available using the test context
+    val updatedExperienceResponse = experienceResponse.copy(
+        steps = experienceResponse.steps.map { stepContainerResponse ->
+            stepContainerResponse.copy(
+                children = stepContainerResponse.children.map { stepResponse ->
+                    stepResponse.copy(
+                        actions = emptyMap()
+                    )
+                },
+                actions = emptyMap()
+            )
+        },
+    )
+
+    // map the experience
+    val experienceMapper: ExperienceMapper = scope.get()
+    val experience = experienceMapper.map(updatedExperienceResponse, ExperienceTrigger.Preview)
+    val container = experience.stepContainers[groupIndex]
+    val metadataSettingTraits = container.steps[stepIndex].metadataSettingTraits
+    val metadata = hashMapOf<String, Any?>().apply { metadataSettingTraits.forEach { putAll(it.produceMetadata()) } }
+
+    AppcuesTheme {
+        CompositionLocalProvider(
+            LocalImageLoader provides imageLoader,
+            LocalLogcues provides Logcues(LoggingLevel.DEBUG),
+            LocalExperienceStepFormStateDelegate provides ExperienceStepFormState(),
+            LocalAppcuesActionDelegate provides FakeAppcuesActionDelegate(),
+            LocalExperienceCompositionState provides ExperienceCompositionState(
+                // disables animations
+                isContentVisible = MutableTransitionState(!animated),
+                isBackdropVisible = MutableTransitionState(!animated)
+            )
+        ) {
+            // render the step container on the desired step
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                ComposeContainer(container, stepIndex, metadata)
+            }
+
+            if (animated) {
+                val compositionState = LocalExperienceCompositionState.current
+                LaunchedEffect(Unit) {
+                    compositionState.isContentVisible.targetState = true
+                    compositionState.isBackdropVisible.targetState = true
+                }
             }
         }
     }
