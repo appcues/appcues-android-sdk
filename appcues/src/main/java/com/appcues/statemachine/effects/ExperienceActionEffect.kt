59ed5b84a79ff42dbb60dbec0d6936bd4afd8ce6
andretortolano <andre@appcues.com>
2023-11-17 15:35:33 -0500

    🐛 ExperienceLaunchEffects should launch different coroutine and unlock processing actions

--- appcues/src/main/java/com/appcues/statemachine/effects/ExperienceActionEffect.kt
+++ appcues/src/main/java/com/appcues/statemachine/effects/ExperienceActionEffect.kt
@@ -8,7 +8,10 @@ import com.appcues.statemachine.SideEffect
 internal data class ExperienceActionEffect(private val actions: List<ExperienceAction>) : SideEffect {
 
     override suspend fun launch(processor: ActionProcessor): Action? {
-        processor.process(actions)
+        // invoking processPostFlowActions ensures the current transition will complete
+        // regardless of the actions being processed here.
+        // This is necessary to avoid a possible deadlock between the actions and the state machine.
+        processor.processPostFlowActions(actions)
 
         return null
     }
