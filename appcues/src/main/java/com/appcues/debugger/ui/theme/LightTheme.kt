7706c9d39f0dbf0e2a8229fe746ebad8bb57f8a1
andretortolano <andre@appcues.com>
2023-12-12 10:24:40 -0500

    👌 Success color and Ajusting Screencapture coloring

--- appcues/src/main/java/com/appcues/debugger/ui/theme/LightTheme.kt
+++ appcues/src/main/java/com/appcues/debugger/ui/theme/LightTheme.kt
@@ -17,6 +17,7 @@ private val Pink600 = Color(color = 0xFFDD2270)
 private val Blurple600 = Color(color = 0xFF5C5CFF)
 private val Blurple700 = Color(color = 0xFF4343C5)
 private val Yellow600 = Color(color = 0xFFA66300)
+private val Green600 = Color(color = 0xFF108484)
 
 // Composing a color palette for light mode are based on foundation figma doc.
 // Visit: https://www.figma.com/file/8Sozh9JnseLTF5oWjvGfIp/Foundations?type=design&node-id=5619-2635&mode=design&t=56CipGcX1bXwpUOW-0
@@ -26,6 +27,7 @@ internal fun lightAppcuesTheme() = AppcuesThemeColors(
     error = Pink600,
     warning = Yellow600,
     info = Blue600,
+    success = Green600,
     link = Blurple700,
     primary = Neutral800,
     secondary = Neutral700,
