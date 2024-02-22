b9d5dda178103ddd68e3589ce4d6932eb296043a
andretortolano <andre@appcues.com>
2024-02-22 14:28:16 -0500

    ✨ Adding _pushToken property + debugger visual

--- appcues/src/main/java/com/appcues/data/remote/appcues/request/EventRequest.kt
+++ appcues/src/main/java/com/appcues/data/remote/appcues/request/EventRequest.kt
@@ -1,5 +1,6 @@
 package com.appcues.data.remote.appcues.request
 
+import com.appcues.data.MoshiConfiguration.SerializeNull
 import com.squareup.moshi.JsonClass
 import java.util.Date
 
@@ -7,6 +8,7 @@ import java.util.Date
 internal data class EventRequest(
     val name: String,
     val timestamp: Date = Date(),
+    @SerializeNull
     val attributes: MutableMap<String, Any> = hashMapOf(),
     val context: MutableMap<String, Any> = hashMapOf()
 )
