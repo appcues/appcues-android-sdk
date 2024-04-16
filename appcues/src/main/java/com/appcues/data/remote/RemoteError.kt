848f862f05e4b963fcce73a2c6df3d0c39648bd7
andretortolano <andre@appcues.com>
2024-04-16 15:32:28 -0400

    ♻️  Improving debugger push check

--- appcues/src/main/java/com/appcues/data/remote/RemoteError.kt
+++ appcues/src/main/java/com/appcues/data/remote/RemoteError.kt
@@ -1,8 +1,10 @@
 package com.appcues.data.remote
 
 import com.appcues.data.remote.appcues.response.ErrorResponse
+import com.appcues.data.remote.appcues.response.PushErrorResponse
 
 internal sealed class RemoteError {
     data class HttpError(val code: Int? = null, val error: ErrorResponse? = null) : RemoteError()
+    data class HttpErrorV2(val code: Int? = null, val error: PushErrorResponse? = null) : RemoteError()
     data class NetworkError(val throwable: Throwable? = null) : RemoteError()
 }
