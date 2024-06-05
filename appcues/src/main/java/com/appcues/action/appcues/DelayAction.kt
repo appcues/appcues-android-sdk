1584490f84f8bf4fc6662b59c71d86a46800e83e
andretortolano <andre@appcues.com>
2024-06-05 16:38:17 -0400

    🐛 using the proper config extension to get duration

--- appcues/src/main/java/com/appcues/action/appcues/DelayAction.kt
+++ appcues/src/main/java/com/appcues/action/appcues/DelayAction.kt
@@ -2,7 +2,7 @@ package com.appcues.action.appcues
 
 import com.appcues.action.ExperienceAction
 import com.appcues.data.model.AppcuesConfigMap
-import com.appcues.data.model.getConfigOrDefault
+import com.appcues.data.model.getConfigInt
 import kotlinx.coroutines.delay
 
 internal class DelayAction(
@@ -14,7 +14,7 @@ internal class DelayAction(
         const val TYPE = "@appcues/delay"
     }
 
-    private val duration = config.getConfigOrDefault("duration", 0)
+    private val duration = config.getConfigInt("duration") ?: 0
 
     override suspend fun execute() {
         delay(duration.toLong())
