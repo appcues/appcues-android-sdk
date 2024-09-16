18a98fe3fa2a87442163f17ec967f607bc321cec
andretortolano <andre@appcues.com>
2024-09-16 15:12:51 -0400

    ♻️  SDK4 branch merge cleanup

--- release.sh
+++ release.sh
@@ -41,11 +41,11 @@ then
 	exit 1
 fi
 
-# check that we're on the `sdk4` or `release` branch
+# check that we're on the `main` or `release` branch
 branch=$(git rev-parse --abbrev-ref HEAD)
-if [[ $branch != 'sdk4' ]] && [[ $branch != release/* ]]
+if [[ $branch != 'main' ]] && [[ $branch != release/* ]]
 then
-	echo "Alpha release from 'sdk4'"
+	echo "The 'main' or 'release/*' must be the current branch to make a release."
 	echo "You are currently on: $branch"
 	exit 1
 fi
