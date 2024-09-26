83bd7deabd25c56156340af67e25ed2755137048
James Ellis <james@appcues.com>
2024-09-26 10:00:41 -0400

    ♻️ Handle negative margin values in our own class since Compose does not allow in PaddingValues any longer

--- appcues/src/main/java/com/appcues/ui/utils/AppcuesMargins.kt
+++ appcues/src/main/java/com/appcues/ui/utils/AppcuesMargins.kt
@@ -11,8 +11,11 @@ import androidx.compose.ui.platform.InspectorInfo
 import androidx.compose.ui.platform.InspectorValueInfo
 import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.constrainHeight
 import androidx.compose.ui.unit.constrainWidth
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.offset
 
 @Stable
@@ -59,4 +62,54 @@ private class MarginValuesModifier(
         val otherModifier = other as? MarginValuesModifier ?: return false
         return paddingValues == otherModifier.paddingValues
     }
+}
+
+// This is a copy/paste of the standard PaddingValuesImpl from compose foundation, but without
+// the restriction against negative values, as we allow negative margins and handle that above
+// in our MarginValuesModifier. Prior to Compose 1.7 we still used the standard PaddingValuesImpl,
+// but in Compose 1.7 they added checks that throw exceptions if negative values are used, so
+// we need our own.
+//
+// This implementation still implements the same PaddingValues interface, so there is minimal impact to
+// existing call sites.
+//
+// the change to check negative values was in this update:
+// https://android-review.googlesource.com/c/platform/frameworks/support/+/3021136/3/compose/foundation/foundation-layout/src/commonMain/kotlin/androidx/compose/foundation/layout/Padding.kt
+@Stable
+@Suppress("FunctionNaming") // matching Compose naming for PaddingValues here
+internal fun MarginValues(all: Dp): PaddingValues = MarginValues(all, all, all, all)
+
+internal class MarginValues(
+    @Stable
+    val start: Dp = 0.dp,
+    @Stable
+    val top: Dp = 0.dp,
+    @Stable
+    val end: Dp = 0.dp,
+    @Stable
+    val bottom: Dp = 0.dp
+) : PaddingValues {
+
+    override fun calculateLeftPadding(layoutDirection: LayoutDirection) =
+        if (layoutDirection == LayoutDirection.Ltr) start else end
+
+    override fun calculateTopPadding() = top
+
+    override fun calculateRightPadding(layoutDirection: LayoutDirection) =
+        if (layoutDirection == LayoutDirection.Ltr) end else start
+
+    override fun calculateBottomPadding() = bottom
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is MarginValues) return false
+        return start == other.start &&
+            top == other.top &&
+            end == other.end &&
+            bottom == other.bottom
+    }
+
+    override fun hashCode() =
+        ((start.hashCode() * 31 + top.hashCode()) * 31 + end.hashCode()) * 31 + bottom.hashCode()
+
+    override fun toString() = "MarginValues(start=$start, top=$top, end=$end, bottom=$bottom)"
 }
